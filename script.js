45a4df8fa49b2e7faf41b0cb0230e072f40073ef
Jazel-Isaac <124520137+Jazel-Isaac@users.noreply.github.com>
2023-06-16 05:09:58 +0300

    updated backend

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 // Add your OpenAI API key here
-const apiKey = "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";
+const apiKey = "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";
 
-document.getElementById("caseForm").addEventListener("submit", async (event) => {
-  event.preventDefault();
-  
-  const caseInput = document.getElementById("caseInput").value;
-  
-  // Make the API request to OpenAI using fetch()
-  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-      prompt: caseInput,
-      max_tokens: 100
-    })
+document.addEventListener("DOMContentLoaded", () => {
+  // Retrieve the form and input elements
+  const caseForm = document.getElementById("caseForm");
+  const caseInput = document.getElementById("caseInput");
+  const summaryResult = document.getElementById("summaryResult");
+
+  // Add submit event listener to the form
+  caseForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const caseText = caseInput.value;
+
+    // Make the API request to OpenAI using fetch()
+    const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: caseText,
+        max_tokens: 100
+      })
+    });
+
+    // Handle the API response
+    const data = await response.json();
+    const summary = data.choices[0].text;
+
+    // Display the summary result
+    summaryResult.innerText = summary;
   });
-
-  // Handle the API response
-  const data = await response.json();
-  const summary = data.choices[0].text;
-  
-  // Display the summary result
-  document.getElementById("summaryResult").innerText = summary;
 });
